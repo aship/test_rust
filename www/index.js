d859b8b51449830933ffd73ecb05a1099f9f0ca6
Yasuo Ashina <ashipp3000@gmail.com>
2020-08-31 12:27:08 +0900

    mandelbrot with Rust WebAssembly

--- www/index.js
+++ www/index.js
@@ -1,3 +1,66 @@
-import * as wasm from "hello-wasm-pack";
+'use strict'
 
-wasm.greet();
+function draw(ctx, canvas_w, canvas_h, data) {
+	let img = new ImageData(new Uint8ClampedArray(data), canvas_w, canvas_h);
+
+	ctx.putImageData(img, 0, 0);
+}
+
+const X_MIN = -1.5;
+const X_MAX = 0.5;
+const Y_MIN = -1.0;
+const Y_MAX = 1.0;
+const MAX_ITER = 64;
+
+console.log("start loading wasm");
+
+const mandelbrot = import('../pkg').catch(console.error)
+
+Promise.all([mandelbrot]).then(async function([
+	{ generate_mandelbrot_set, draw_mandelbrot_set }
+]) {
+	console.log("fifnished loading wasm");
+
+	const renderBtn = document.getElementById('render');
+
+	renderBtn.addEventListener('click', () => {
+		draw_mandelbrot_set();
+
+		let wasmResult = null;
+
+		{
+			const CANVAS_ID = "canvas_hybrid"
+			let canvas = document.getElementById(CANVAS_ID);
+			let context = canvas.getContext("2d");
+			const canvasWidth = canvas.width;
+			const canvasHeight = canvas.height;
+
+			const generateStartTime = Date.now();
+			wasmResult = generate_mandelbrot_set(
+				canvasWidth,
+				canvasHeight,
+				X_MIN,
+				X_MAX,
+				Y_MIN,
+				Y_MAX,
+				MAX_ITER);
+
+			const generateEndTime = Date.now();
+			const drawStartTime = Date.now();
+
+			draw(context, canvasWidth, canvasHeight, wasmResult);
+
+			const drawEndTime = Date.now();
+			const elapsed = generateEndTime - generateStartTime;
+			console.log(`\t generate: wasm \t generate_elapsed:${elapsed}[ms]`);
+			console.log(`\t draw: js \t draw_elapsed:${drawEndTime - drawStartTime}[ms]`);
+		}
+	});
+});
+
+
+
+
+
+
+
